676a111d6f0392e507548917b436b82066201796
yuansheng-sun <yuansheng.sun@hpe.com>
2018-10-29 16:49:46 +0800

    realize zuul interceptor

--- zuul/src/main/java/com/sys/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/sys/zuul/ZuulApplication.java
@@ -1,12 +1,23 @@
 package com.sys.zuul;
 
+import com.sys.zuul.filters.Filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@Bean
+	public Filter getFilter(){
+		return new Filter();
 	}
 }
---
+++ zuul/src/main/java/com/sys/zuul/filters/Filter.java
@@ -0,0 +1,38 @@
+package com.sys.zuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Filter extends ZuulFilter{
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String accessToken = String.valueOf(request.getParameter("Token"));
+        if(!accessToken.equals("zz")) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        return null;
+    }
+}
